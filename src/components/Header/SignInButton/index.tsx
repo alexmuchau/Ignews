be6787abfafac33e426848c1277cd0df30897bac
Alex Muchau <srrplays@gmail.com>
2022-05-31 16:33:06 -0300

    componentization-and-styles

---
+++ src/components/Header/SignInButton/index.tsx
@@ -0,0 +1,27 @@
+import { FiX } from 'react-icons/fi'
+import { FaGithub } from "react-icons/fa" 
+
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+  const isUserLoggedIn = true
+
+  return isUserLoggedIn ? (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#04d361" />
+      Alex Muchau
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#eba417" />
+      Sign In with Github
+    </button>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import Head from '../../node_modules/next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss'
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <>
+     <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero} >
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span></h1>
+          <p>
+            Get acess to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
+    </>
+  )
 }
